bfd72d2ab1d5b895e63f6bf61f66bde0604f36e1
Karun Agarwal <113603846+singhalkarun@users.noreply.github.com>
2024-02-09 16:11:38 +0530

    Update pds.sql

--- pds.sql
+++ pds.sql
@@ -1,4 +1,4 @@
-CREATE MATERIALIZED VIEW pds_hs_availing
+CREATE MATERIALIZED VIEW IF NOT EXISTS pds_hs_availing
   AS
 SELECT COUNT(DISTINCT bd."rationCardNumber") AS distinct_rationcard_count
 FROM external_beneficiary_detail bd
@@ -7,4 +7,28 @@ WHERE bd."rationCardNumber" IN (
   FROM undefinednfsamemberdatashare n
   UNION -- Removes duplicates between the two scheme tables
   SELECT s."rationcard_number"
-  FROM undefinedsfssmemberdatashare s);;
+  FROM undefinedsfssmemberdatashare s);
+
+CREATE MATERIALIZED VIEW IF NOT EXISTS pds_district_progress
+  AS
+select "districtName",
+"Number of Eligible Households",
+"No. of Households who have availed scheme(s)" * 1000,
+ROUND(CAST("Saturation %"AS decimal),1) as "Saturation %",
+  ROW_NUMBER () OVER(ORDER BY "Saturation %" DESC) AS "Rank"
+from
+(SELECT
+  bd."districtName",
+  COUNT(DISTINCT bd."rationCardNumber") AS "Number of Eligible Households",
+  COUNT(DISTINCT nd."rationcard_number") AS "No. of Households who have availed scheme(s)",
+  FORMAT(
+    (COUNT(DISTINCT CASE WHEN nd."rationcard_number" IS NOT NULL THEN nd."rationcard_number" END) * 100.00) / 
+    NULLIF(COUNT(DISTINCT bd."rationCardNumber"), 0), 2
+  ) AS "Saturation %"
+FROM
+  external_beneficiary_detail bd
+LEFT JOIN
+  undefinednfsamemberdatashare nd ON bd."rationCardNumber" = nd."rationcard_number"
+GROUP BY
+  bd."districtName") as X
+  order by "Rank" ASC;
